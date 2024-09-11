4cad872e511ddaa29f67cdc04a4f1ad6beb78fd5
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-12 00:59:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 let currentAudio;
 
 function playSound(sound) {
+
+	const sounds={
+		
+        applause: "https://www.fesliyanstudios.com/play-mp3/387",
+        boo: "https://www.fesliyanstudios.com/play-mp3/421",
+        gasp: "https://www.fesliyanstudios.com/play-mp3/477",
+        tada: "https://www.soundbible.com/mp3/Ta%20Da-SoundBible.com-1884170640.mp3",
+        victory: "https://www.fesliyanstudios.com/play-mp3/667",
+        wrong: "https://www.fesliyanstudios.com/play-mp3/456",
+        stop: "https://www.fesliyanstudios.com/play-mp3/414"
+    };
+	
+
     if (currentAudio) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
     }
 
-    currentAudio = new Audio(`sounds/${sound}.mp3`);
+    currentAudio = new Audio(sounds[sound]);
     currentAudio.play();
-}
-
-function stopSound() {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-    }
 }
