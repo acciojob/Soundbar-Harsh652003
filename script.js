ab902df6d0802a5f3acf8ace90fd2bb58a61a1e3
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-12 00:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let currentAudio;
+
+function playSound(sound) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${sound}.mp3`);
+    currentAudio.play();
+}
+
+function stopSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+}
