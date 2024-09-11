62e9795c18988f98623dc2f2b69a7d18bbe621ad
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-12 01:04:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function playSound(sound) {
         gasp: "https://www.fesliyanstudios.com/play-mp3/477",
         tada: "https://www.soundbible.com/mp3/Ta%20Da-SoundBible.com-1884170640.mp3",
         victory: "https://www.fesliyanstudios.com/play-mp3/667",
-        wrong: "https://www.fesliyanstudios.com/play-mp3/456",
-        stop: "https://www.fesliyanstudios.com/play-mp3/414"
+        wrong: "https://www.fesliyanstudios.com/play-mp3/456"
+        // stop: "https://www.fesliyanstudios.com/play-mp3/414"
     };
 	
 
@@ -22,4 +22,11 @@ function playSound(sound) {
 
     currentAudio = new Audio(sounds[sound]);
     currentAudio.play();
+}
+
+function stopSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
 }
