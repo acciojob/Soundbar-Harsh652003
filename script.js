1d7fc4fe589e6e7b817739ac0b5ee1796fa92a17
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-12 23:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-//your JS code here. If required.
-let currentAudio;
-
-function playSound(sound) {
-
-	const sounds={
-		
-        applause: "https://www.fesliyanstudios.com/play-mp3/387",
-        boo: "https://www.fesliyanstudios.com/play-mp3/421",
-        gasp: "https://www.fesliyanstudios.com/play-mp3/477",
-        tada: "https://www.soundbible.com/mp3/Ta%20Da-SoundBible.com-1884170640.mp3",
-        victory: "https://www.fesliyanstudios.com/play-mp3/667",
-        wrong: "https://www.fesliyanstudios.com/play-mp3/456"
-        // stop: "https://www.fesliyanstudios.com/play-mp3/414"
-    };
-	
-
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
+ 
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.btn');
+    const stopButton = document.querySelector('.stop');
+    const audioPlayer = document.getElementById('audioPlayer');
+ 
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const soundFile = this.getAttribute('data-sound');
+            playSound(soundFile);
+        });
+    });
+ 
+    stopButton.addEventListener('click', function() {
+        stopSound();
+    });
+ 
+    function playSound(soundFile) {
+        audioPlayer.src = soundFile;
+        audioPlayer.play();
     }
-
-    currentAudio = new Audio(sounds[sound]);
-    currentAudio.play();
-}
-
-function stopSound() {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
+ 
+    function stopSound() {
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
     }
-}
+});
